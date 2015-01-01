91cef23bf28455d610e443b954f41ba0b2143fa1
Jake Albaugh <jake.albaugh@gmail.com>
2015-01-01 16:18:32 -0600

    Initial Setup
    Off to the races

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    bower_concat: {
+      build: {
+        dest: 'src/scripts/_bower.js'
+      }
+    },
+    copy: {
+      html: {
+        files: [
+          {expand: true, flatten: false, cwd: 'src/html', src: ['*'], dest: 'build', filter: 'isFile'},
+        ]
+      }
+    },
+    uglify: {
+      app: {
+        files: {
+          'build/js/app.min.js': ['src/scripts/app.js']
+        }
+      },
+      bower: {
+        files: {
+          'build/js/head.min.js': ['src/scripts/_bower.js']
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        options: {
+          join: true
+        },
+        files: {
+          'src/scripts/app.js': ['src/scripts/coffee/app.coffee', 'src/scripts/coffee/**/*.coffee', 'src/scripts/coffee/*.coffee']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'src/styles/main.css': 'src/styles/scss/main.scss'
+        }
+      }
+    },
+    autoprefixer: {
+      overwrite: {
+        src: 'src/styles/main.css'
+      }
+    },
+    cssmin: {
+      compile: {
+        files: {
+          'build/css/main.min.css': ['src/styles/main.css']
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['src/scripts/coffee/**/*.coffee', 'src/scripts/coffee/*.coffee'],
+        tasks: ['script'],
+        options: {
+          livereload: true
+        }
+      },
+      bower: {
+        files: ['bower_components/**/*.js'],
+        tasks: ['bowerjs'],
+        options: {
+          livereload: true
+        }
+      },
+      html: {
+        files: ['src/html/*.html'],
+        tasks: ['html'],
+        options: {
+          livereload: true
+        }
+      },
+      css: {
+        files: ['src/styles/scss/**/*.scss', 'src/styles/scss/*.scss'],
+        tasks: ['style'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          livereload: true,
+          base: 'build/',
+          port: 9000
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('style',   ['sass:dist', 'autoprefixer:overwrite', 'cssmin:compile']);
+  grunt.registerTask('script',  ['coffee:compile', 'uglify:app']);
+  grunt.registerTask('html',    ['copy:html']);
+  grunt.registerTask('bowerjs', ['bower_concat:build', 'uglify:bower']);
+
+  grunt.registerTask('build',   [ 'style', 'script', 'html', 'bowerjs' ]);
+  grunt.registerTask('serve',   [ 'build', 'connect:server', 'watch' ]);
+
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+#Random Commander
+Here we go.
+
+- cd to directory
+- run `npm install`
+- run `bower install`
+- run `grunt serve`
